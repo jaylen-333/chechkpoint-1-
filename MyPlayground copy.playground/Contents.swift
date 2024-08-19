511e0948e0d21aa551adfb8f56e13e5c88a05491
jaylen <jaylen.thomas@stu.barren.kyschools.us>
2024-08-19 09:13:58 -0500

    Initial Commit

---
+++ MyPlayground copy.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+//1. create a constant holding any tempature in celsius
+let celsiusTemperature: Double = 25.0
+
+//2.Convert the Celsius temperature to Fahrenheit
+let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32
+
+//3. Print the result for the user showing both the Celsius and Fahrenheit values
+print("The Temperature is \(celsiusTemperature)°C, which is \(fahrenheitTemperature)°F.")
